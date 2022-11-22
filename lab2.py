eccb2348b7fa85f7a1f7872f8b12b828c071bc11
Neil94OS <113621417+Neil94OS@users.noreply.github.com>
2022-11-22 20:39:02 +0000

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 27 13:16:37 2022
+
+@author: neil o sullivan
+"""
+
+###########Question 1 ################
+
+def q1():
+   print("Hello World")
+
+
+
+
+
+q1()
+
+
+
